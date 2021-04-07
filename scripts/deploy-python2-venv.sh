cd6f3421dc0cb1e69f265235bd913d08ee6b10d9
Your Name <you@example.com>
2021-04-07 12:49:46 +0300

    La fraude

--- scripts/deploy-python2-venv.sh
+++ scripts/deploy-python2-venv.sh
@@ -8,11 +8,11 @@ set -o xtrace
 echo "============================deploy python 2x virtual environment============================================================="
     
 python -V #Python 3.8.5
-python2 --version #Python 2.7.18
 python3 -V #Python 3.8.5
+python2 --version #Python 2.7.18
 python3 --version #Python 3.8.5
-pip --version
-pip3 --version
+pip --version #pip 20.0.2 from /usr/lib/python3/dist-packages/pip (python 3.8
+pip3 --version #pip 20.0.2 from /usr/lib/python3/dist-packages/pip (python 3.8)
 
 apt-get update -qq
 # sudo apt-get install python-pip -yqq #Unable to locate package python-pip
@@ -24,8 +24,12 @@ virtualenv -p /usr/bin/python2.7 ~venv27
 
 source ~venv27/bin/activate
 pip install -r requirements.txt
-pip --version
-pip3 --version
+python -V #Python 3.8.5
+python3 -V #Python 3.8.5
+python2 --version #Python 2.7.18
+python3 --version #Python 3.8.5
+pip --version #pip 20.3.4 from /home/runner/work/kind-githubactions/kind-githubactions/~venv27/lib/python2.7/site-packages/pip (python 2.7)
+pip3 --version #pip 20.0.2 from /usr/lib/python3/dist-packages/pip (python 3.8)
 
 deactivate 
 
