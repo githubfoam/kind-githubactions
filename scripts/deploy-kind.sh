c0c50da0191cb1fffea5f437e32a3f48638ec4f7
Your Name <you@example.com>
2022-02-11 13:54:46 +0300

    worker

--- scripts/deploy-kind.sh
+++ scripts/deploy-kind.sh
@@ -15,7 +15,17 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-$(uname)-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 
+# create two clusters
+kind create cluster # Default cluster context name is `kind`.
+kind create cluster --name kind-2
+
 kind get clusters #see the list of kind clusters
+
+kubectl cluster-info --context kind-kind
+kubectl cluster-info --context kind-kind-2
+
 kubectl config get-contexts #kind is prefixed to the context and cluster names, for example: kind-istio-testing
+
+kind delete kind-2
 
 echo "=============================deploy kind============================================================="
